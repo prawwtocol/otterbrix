66d148157d69525a8d06a1a2c1a936bcbffd909e
Mikhail Fedorenko <80476811+madoxann@users.noreply.github.com>
2025-11-17 17:00:30 +0300

    Parameter binding improvements (#440)

--- components/sql/test/test_parameter.cpp
+++ components/sql/test/test_parameter.cpp
@@ -16,22 +16,27 @@ using namespace components::expressions;
 using v = types::logical_value_t;
 using vec = std::vector<v>;
 
-#define TEST_PARAMS(QUERY, RESULT, BIND)                                                                               \
+#define TEST_PARAMS_SELECT(QUERY, RESULT, BIND)                                                                        \
     {                                                                                                                  \
         SECTION(QUERY) {                                                                                               \
             auto select = linitial(raw_parser(&arena_resource, QUERY));                                                \
             auto binder = transformer.transform(pg_cell_to_node_cast(select));                                         \
-            for (auto i = 0ul; i < BIND.size(); ++i) {                                                                 \
-                binder.bind(i + 1, BIND.at(i));                                                                        \
-            }                                                                                                          \
-            auto result = std::get<result_view>(binder.finalize());                                                    \
-            auto node = result.node;                                                                                   \
-            auto agg = result.params;                                                                                  \
-            REQUIRE(node->to_string() == RESULT);                                                                      \
-            REQUIRE(agg->parameters().parameters.size() == BIND.size());                                               \
-            for (auto i = 0ul; i < BIND.size(); ++i) {                                                                 \
-                REQUIRE(agg->parameter(core::parameter_id_t(uint16_t(i))) == BIND.at(i));                              \
-            }                                                                                                          \
+            TEST_PARAMS(RESULT, BIND)                                                                                  \
+        }                                                                                                              \
+    }
+
+#define TEST_PARAMS(RESULT, BIND)                                                                                      \
+    {                                                                                                                  \
+        for (auto i = 0ul; i < BIND.size(); ++i) {                                                                     \
+            binder.bind(i + 1, BIND.at(i));                                                                            \
+        }                                                                                                              \
+        auto result = std::get<result_view>(binder.finalize());                                                        \
+        auto node = result.node;                                                                                       \
+        auto agg = result.params;                                                                                      \
+        REQUIRE(node->to_string() == RESULT);                                                                          \
+        REQUIRE(agg->parameters().parameters.size() == BIND.size());                                                   \
+        for (auto i = 0ul; i < BIND.size(); ++i) {                                                                     \
+            REQUIRE(agg->parameter(core::parameter_id_t(uint16_t(i))) == BIND.at(i));                                  \
         }                                                                                                              \
     }
 
@@ -59,17 +64,18 @@ TEST_CASE("sql::select_bind") {
     std::pmr::monotonic_buffer_resource arena_resource(&resource);
     transform::transformer transformer(&resource);
 
-    TEST_PARAMS(R"_(SELECT * FROM TestDatabase.TestCollection WHERE number = $1 AND name = $2 AND "count" = $1;)_",
-                R"_($aggregate: {$match: {$and: ["number": {$eq: #0}, "name": {$eq: #1}, "count": {$eq: #0}]}})_",
-                vec({v(10l), v(std::string("doc 10"))}));
+    TEST_PARAMS_SELECT(
+        R"_(SELECT * FROM TestDatabase.TestCollection WHERE number = $1 AND name = $2 AND "count" = $1;)_",
+        R"_($aggregate: {$match: {$and: ["number": {$eq: #0}, "name": {$eq: #1}, "count": {$eq: #0}]}})_",
+        vec({v(10l), v(std::string("doc 10"))}));
 
-    TEST_PARAMS(R"_(SELECT * FROM TestDatabase.TestCollection WHERE number = $1 OR name = $2;)_",
-                R"_($aggregate: {$match: {$or: ["number": {$eq: #0}, "name": {$eq: #1}]}})_",
-                vec({v(42l), v(std::string("abc"))}));
+    TEST_PARAMS_SELECT(R"_(SELECT * FROM TestDatabase.TestCollection WHERE number = $1 OR name = $2;)_",
+                       R"_($aggregate: {$match: {$or: ["number": {$eq: #0}, "name": {$eq: #1}]}})_",
+                       vec({v(42l), v(std::string("abc"))}));
 
-    TEST_PARAMS(R"_(SELECT * FROM TestDatabase.TestCollection WHERE id > $1 AND flag = $2;)_",
-                R"_($aggregate: {$match: {$and: ["id": {$gt: #0}, "flag": {$eq: #1}]}})_",
-                vec({v(5l), v(true)}));
+    TEST_PARAMS_SELECT(R"_(SELECT * FROM TestDatabase.TestCollection WHERE id > $1 AND flag = $2;)_",
+                       R"_($aggregate: {$match: {$and: ["id": {$gt: #0}, "flag": {$eq: #1}]}})_",
+                       vec({v(5l), v(true)}));
 }
 
 TEST_CASE("sql::update_bind") {
@@ -144,7 +150,11 @@ TEST_CASE("sql::insert_bind") {
         auto query = R"_(INSERT INTO TestDatabase.TestCollection (id, parent_id) VALUES ($1, $1);)_";
         auto stmt = linitial(raw_parser(&arena_resource, query));
         auto binder = transformer.transform(pg_cell_to_node_cast(stmt));
+
+        REQUIRE(binder.all_bound() == false);
         binder.bind(1, v(123l));
+        REQUIRE(binder.all_bound() == true);
+
         auto result = std::get<result_view>(binder.finalize());
         auto node = result.node;
 
@@ -196,11 +206,52 @@ TEST_CASE("sql::transform_result") {
         REQUIRE(std::holds_alternative<transform::bind_error>(result));
     }
 
-    SECTION("finalize") {
-        auto stmt = linitial(raw_parser(&arena_resource, "SELECT * FROM TestDatabase.TestCollection;"));
+    SECTION("reuse binder") {
+        auto query = R"_(SELECT * FROM TestDatabase.TestCollection WHERE number = $1 AND name = $2 AND "count" = $1;)_";
+        auto select = linitial(raw_parser(&arena_resource, query));
+        auto binder = transformer.transform(pg_cell_to_node_cast(select));
+        TEST_PARAMS(R"_($aggregate: {$match: {$and: ["number": {$eq: #0}, "name": {$eq: #1}, "count": {$eq: #0}]}})_",
+                    vec({v(10l), v(std::string("doc 10"))}));
+
+        TEST_PARAMS(R"_($aggregate: {$match: {$and: ["number": {$eq: #0}, "name": {$eq: #1}, "count": {$eq: #0}]}})_",
+                    vec({v(3.14), v(std::string("another doc 10"))}));
+
+        TEST_PARAMS(R"_($aggregate: {$match: {$and: ["number": {$eq: #0}, "name": {$eq: #1}, "count": {$eq: #0}]}})_",
+                    vec({v(false), v(std::string("another another doc 10"))}));
+    }
+
+    SECTION("intrusive ptr update") {
+        auto resource = std::pmr::synchronized_pool_resource();
+        std::pmr::monotonic_buffer_resource arena_resource(&resource);
+        transform::transformer transformer(&resource);
+
+        auto query = R"_(SELECT * FROM TestDatabase.TestCollection WHERE number = $1;)_";
+        auto select = linitial(raw_parser(&arena_resource, query));
+        auto binder = transformer.transform(pg_cell_to_node_cast(select));
+
+        binder.bind(1, v("doc"));
+        auto agg = std::get<result_view>(binder.finalize()).params;
+        REQUIRE(agg->parameter(core::parameter_id_t(uint16_t(0))) == v("doc"));
+
+        binder.bind(1, v(100l)).finalize();
+        REQUIRE(agg->parameter(core::parameter_id_t(uint16_t(0))) == v(100l));
+    }
+
+    SECTION("reuse insert binding") {
+        auto query = R"_(INSERT INTO TestDatabase.TestCollection (id, parent_id) VALUES ($1, $2);)_";
+        auto stmt = linitial(raw_parser(&arena_resource, query));
         auto binder = transformer.transform(pg_cell_to_node_cast(stmt));
-        auto result = binder.finalize();
-        REQUIRE(std::holds_alternative<transform::result_view>(result));
-        REQUIRE(std::holds_alternative<transform::bind_error>(binder.finalize()));
+        binder.bind(1, v(123l)).bind(2, v(false));
+        auto node = std::get<result_view>(binder.finalize()).node;
+
+        const auto& keys = reinterpret_cast<logical_plan::node_insert_ptr&>(node)->key_translation();
+        binder.bind(1, v(true)).bind(2, v(std::string("doc 10"))).finalize();
+
+        const auto& chunk =
+            reinterpret_cast<components::logical_plan::node_data_ptr&>(node->children().front())->data_chunk();
+        REQUIRE(chunk.size() == 1);
+        REQUIRE(chunk.value(0, 0) == v(true));
+        REQUIRE(chunk.value(1, 0) == v(std::string("doc 10")));
+        REQUIRE(reinterpret_cast<logical_plan::node_insert_ptr&>(node)->key_translation() == keys);
     }
 }
--- components/sql/transformer/transform_result.hpp
+++ components/sql/transformer/transform_result.hpp
@@ -3,6 +3,7 @@
 #include <components/document/document.hpp>
 #include <components/expressions/key.hpp>
 #include <components/logical_plan/node.hpp>
+#include <components/logical_plan/node_data.hpp>
 #include <components/logical_plan/node_insert.hpp>
 #include <components/logical_plan/param_storage.hpp>
 #include <components/sql/parser/nodes/parsenodes.h>
@@ -36,7 +37,11 @@ namespace components::sql::transform {
                          logical_plan::parameter_node_ptr&& params,
                          parameter_map_t&& param_map,
                          insert_map_t&& param_insert_map,
-                         insert_rows_t&& param_insert_docs);
+                         insert_rows_t&& param_insert_rows);
+        transform_result(const transform_result&) = delete;
+        transform_result& operator=(const transform_result&) = delete;
+        transform_result(transform_result&&) = default;
+        transform_result& operator=(transform_result&&) = default;
 
         template<typename T>
         transform_result& bind(size_t id, T&& value) {
@@ -48,7 +53,18 @@ namespace components::sql::transform {
                 return *this;
             }
 
+            bool prev_finalized = std::exchange(finalized_, false);
             if (node_->type() == logical_plan::node_type::insert_t) {
+                if (prev_finalized) {
+                    // first bind after finalize - restore "binding" state of data node
+                    // cannot move rows out of data node - copy
+                    const auto& rows =
+                        reinterpret_cast<logical_plan::node_data_ptr&>(node_->children().front())->data_chunk();
+                    vector::data_chunk_t new_rows(rows.resource(), rows.types(), rows.size());
+                    rows.copy(new_rows);
+                    param_insert_rows_ = std::move(new_rows);
+                }
+
                 auto it = param_insert_map_.find(id);
                 if (it == param_insert_map_.end()) {
                     last_error_ = bind_error("Parameter with id=" + std::to_string(id) + " not found");
@@ -66,6 +82,12 @@ namespace components::sql::transform {
                         param_insert_rows_.data.emplace_back(param_insert_rows_.resource(),
                                                              value.type(),
                                                              param_insert_rows_.capacity());
+                    } else if (column->type() != value.type()) {
+                        // column was inserted before, however type has changed
+                        value.set_alias(key);
+                        *column = vector::vector_t(param_insert_rows_.resource(),
+                                                   value.type(),
+                                                   param_insert_rows_.capacity());
                     }
                     param_insert_rows_.set_value(column_index, i, std::move(value));
                 }
@@ -83,6 +105,10 @@ namespace components::sql::transform {
             return *this;
         }
 
+        logical_plan::node_ptr node_ptr() const;
+
+        logical_plan::parameter_node_ptr params_ptr() const;
+
         size_t parameter_count() const;
 
         bool all_bound() const;
@@ -90,6 +116,8 @@ namespace components::sql::transform {
         std::variant<result_view, bind_error> finalize();
 
     private:
+        using key_translation_t = std::pmr::vector<std::pair<expressions::key_t, expressions::key_t>>;
+
         logical_plan::node_ptr node_;
         logical_plan::parameter_node_ptr params_;
         parameter_map_t param_map_;
@@ -99,6 +127,7 @@ namespace components::sql::transform {
         logical_plan::storage_parameters taken_params_;
         std::pmr::unordered_map<size_t, bool> bound_flags_;
         bind_error last_error_;
+        bool finalized_;
     };
 
 } // namespace components::sql::transform
