6cacc3bffdf94f55a145571f9647a70654a2a403
seliverstow <seliverstow@yandex-team.ru>
2026-02-11 11:16:34 +0300

    fix(ci): satisfy -Werror in pythonpkg integration files

--- integration/python/components/function/table_function.cpp
+++ integration/python/components/function/table_function.cpp
@@ -10,6 +10,6 @@ namespace components::function {
 
     TableFunction::TableFunction(std::string name, std::vector<types::complex_logical_type> arguments, table_function_t function,
             table_function_bind_t bind, table_function_init_global_t init_global, table_function_init_local_t init_local) 
-        : SimpleNamedParameterFunction(std::move(name), std::move(arguments)), function(function), bind(bind)
-        , init_global(init_global), init_local(init_local) {}
+        : SimpleNamedParameterFunction(std::move(name), std::move(arguments)),
+          bind(bind), init_global(init_global), init_local(init_local), function(function) {}
 } // namespace components::function
