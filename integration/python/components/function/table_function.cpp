c1d2da6d9c1cd92d5f8138e9eb2a9adabf000138
seliverstow <seliverstow@yandex-team.ru>
2026-05-06 15:32:16 +0300

    refactor(function): update Equals method and constructor initialization for clarity

--- integration/python/components/function/table_function.cpp
+++ integration/python/components/function/table_function.cpp
@@ -9,7 +9,7 @@ namespace components::function {
     LocalTableFunctionState::~LocalTableFunctionState() = default;
 
     TableFunction::TableFunction(std::string name, std::vector<types::complex_logical_type> arguments, table_function_t function,
-            table_function_bind_t bind, table_function_init_global_t init_global, table_function_init_local_t init_local) 
-        : SimpleNamedParameterFunction(std::move(name), std::move(arguments)), function(function), bind(bind)
-        , init_global(init_global), init_local(init_local) {}
+            table_function_bind_t bind, table_function_init_global_t init_global, table_function_init_local_t init_local)
+        : SimpleNamedParameterFunction(std::move(name), std::move(arguments)), bind(bind)
+        , init_global(init_global), init_local(init_local), function(function) {}
 } // namespace components::function
