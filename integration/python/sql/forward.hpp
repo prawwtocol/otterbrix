7dc348d73a4c8a6565515d9453ccfc8a451c1df9
seliverstow <seliverstow@yandex-team.ru>
2026-05-14 13:46:54 +0300

    import: move pythonpkg tree into integration/python and core; align tree to b82fc4d0

--- integration/python/sql/forward.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <boost/smart_ptr/intrusive_ptr.hpp>
-#include <components/session/session.hpp>
-
-namespace otterbrix {
-    using components::session::session_id_t;
-
-} // namespace otterbrix
