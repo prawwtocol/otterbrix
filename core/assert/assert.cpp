6ab9f0632a1469cbda1f0f83dc3dc3863b8ed48e
seliverstow <seliverstow@yandex-team.ru>
2026-05-16 15:56:58 +0300

    fix(assert): add missing newline in assert.cpp for code clarity
    Inserted a newline in the assert.cpp file to improve code readability and maintain consistent formatting within the file.

--- core/assert/assert.cpp
+++ core/assert/assert.cpp
@@ -10,6 +10,7 @@
 #include "trace_full_exception.hpp"
 
 namespace core::detail {
+
     class InvariantError : public trace_full_exception {
         using trace_full_exception::trace_full_exception;
     };
